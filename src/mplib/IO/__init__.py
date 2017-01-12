ff195af31d655e5d8e7d57fc15f6cdfb645190bb
潘俊俊 <keyps2@icloud.com>
2017-01-12 23:27:09 +0800

    改善库的调用逻辑

--- src/mplib/IO/__init__.py
+++ src/mplib/IO/__init__.py
@@ -1,2 +1,7 @@
 # coding: utf-8
 # __author__: u"John"
+from mplib.IO.db.hive.connector import Hive
+from mplib.IO.db.mysql.connector import MPMySQL
+from mplib.IO.db.postgresql.connector import MPPG
+from mplib.IO.db.redis.connector import MPRedis
+from mplib.IO.file.binary.pickle_helper import pickle_load, pickle_dump
--- src/mplib/common/__init__.py
+++ src/mplib/common/__init__.py
@@ -5,7 +5,6 @@ from base_class import AttributeDict
 # endregion
 
 # region function import
-from mplib.IO.file.binary.pickle_helper import pickle_dump, pickle_load
-from decorators import time_elapse
-from unicode_tools import to_unicode, change_charset, decode, encode
+from decorators import time_elapse, clock_elapse
+from unicode_tools import to_unicode, change_charset, smart_decode, smart_encode
 # endregion
